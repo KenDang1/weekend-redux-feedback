74d1c1ab5a4f1e3fc682d710072398d7419994a1
Ken Dang <k3ndang@gmail.com>
2022-01-22 14:30:33 -0600

    created a header component, and also import router in app.jax

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+function Header () {
+    return (
+        <>
+        {/** I just moved this from App.jsx */}
+        <header className='App-header'>
+            <h1 className='App-title'>Feedback!</h1>
+            <h4>Don't forget it!</h4>
+        </header>
+        </>
+    )
+}; // end of Header function
+
+
+// export this function to App.jsx
+export default Header;
